734f1e5f944abc45682e26bee2bba875d53ea7a3
StephanoAH <stephanoah@protonmail.com>
2021-07-19 17:32:04 -0400

    todo table

--- app/models.py
+++ app/models.py
@@ -2,8 +2,15 @@ import datetime
 from uuid import uuid4
 
 from .db.base import Base
-from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 
 
+class Todo(Base):
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
+    title = Column(String)
+    description = Column(String)
+    status = Column(Boolean, default=False)
+    created_on = Column(Date, default=datetime.datetime.now)
+    updated_on = Column(Date, default=datetime.datetime.now, onupdate=datetime.datetime.now)
